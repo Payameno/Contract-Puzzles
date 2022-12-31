53362cab163f455ebc460dd2ee7c31305354ef15
Payam Beigi <beigi.payam@gmail.com>
2022-12-30 18:44:38 -0800

    Won all games

--- test/game5Test.js
+++ test/game5Test.js
@@ -6,14 +6,36 @@ describe('Game5', function () {
     const Game = await ethers.getContractFactory('Game5');
     const game = await Game.deploy();
 
-    return { game };
+    const threshold = "0x00FfFFfFFFfFFFFFfFfFfffFFFfffFfFffFfFFFf";
+    let validAddress;
+    let wallet;
+    let address;
+    
+    while(!validAddress) {
+      const randomWallet = ethers.Wallet.createRandom();
+      const walletAddress = await randomWallet.getAddress();
+      
+      //Javascript converting string to number and comparing
+      if (walletAddress < threshold) {
+        console.log("Valid Address Found", walletAddress);
+        
+        wallet = randomWallet;
+        address = walletAddress;
+        validAddress = true;
+      }
+    }
+    
+    //Add balance to the valid address for tx gas fee
+    wallet = wallet.connect(ethers.provider);
+    const signer = ethers.provider.getSigner(0);
+    await signer.sendTransaction({ to: address, value: ethers.utils.parseEther("200")})
+  
+    return { game, wallet };
   }
   it('should be a winner', async function () {
-    const { game } = await loadFixture(deployContractAndSetVariables);
+    const { game, wallet } = await loadFixture(deployContractAndSetVariables);
 
-    // good luck
-
-    await game.win();
+    await game.connect(wallet).win();
 
     // leave this assertion as-is
     assert(await game.isWon(), 'You did not win the game');
